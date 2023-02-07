e8daabdb1f8ddefec56b06848226691c95544e0f
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-07 15:00:29 +0100

    hotels on page inside bootstrap table

--- index.php
+++ index.php
@@ -40,14 +40,8 @@
 
     ];
 
-foreach($hotels as $info_hotel){
-        echo $info_hotel['name'] . '<br/>';
-        echo $info_hotel['description'] . '<br/>';
-        echo $info_hotel['parking'] . '<br/>';
-        echo $info_hotel['vote'] . '<br/>';
-        echo $info_hotel['distance_to_center'] . '<br/>';
-    };
-?>
+?> 
+
 
 
 <!DOCTYPE html>
@@ -61,7 +55,31 @@ foreach($hotels as $info_hotel){
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
+
     <div class="container">
+    <table class="table table-hover mt-5">
+        <thead>
+            <tr>
+                <th scope="col">NOME</th>
+                <th scope="col">DESCRIZIONE</th>
+                <th scope="col">PARCHEGGIO</th>
+                <th scope="col">VOTO</th>
+                <th scope="col">DISTANZA DAL CENTRO</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($hotels as $value) : ?>
+            <tr>
+            <td> <?= $value['name'] ?> </td>
+            <td> <?= $value['description'] ?> </td>
+            <td> <?= $value['parking'] ? 'si' : 'no' ?> </td>
+            <td> <?= $value['vote'] ?>/5</td>
+            <td> <?= $value['distance_to_center'] ?> </td>
+            </tr>
+        </tbody>
+        <?php endforeach; ?>
+    </table>
     </div>
+    
 </body>
 </html>
